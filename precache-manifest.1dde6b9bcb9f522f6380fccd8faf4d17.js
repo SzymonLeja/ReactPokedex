3b48ed568a8910f5bc1177d65d38c642b79c7cc9
Sleja <szymon.leja3@gmail.com>
2019-07-01 19:36:37 +0200

    Updates

---
+++ precache-manifest.1dde6b9bcb9f522f6380fccd8faf4d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424e447514147a09ca4f4a648e32c35f",
+    "url": "/ReactPokedex/index.html"
+  },
+  {
+    "revision": "28948508f73dc0389495",
+    "url": "/ReactPokedex/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6238c103636b303308d6",
+    "url": "/ReactPokedex/static/css/main.e7e92bb8.chunk.css"
+  },
+  {
+    "revision": "28948508f73dc0389495",
+    "url": "/ReactPokedex/static/js/2.5f459603.chunk.js"
+  },
+  {
+    "revision": "6238c103636b303308d6",
+    "url": "/ReactPokedex/static/js/main.0f0434fa.chunk.js"
+  },
+  {
+    "revision": "21b77e8ddff556ea4371",
+    "url": "/ReactPokedex/static/js/runtime~main.77a1f957.js"
+  }
+]);
